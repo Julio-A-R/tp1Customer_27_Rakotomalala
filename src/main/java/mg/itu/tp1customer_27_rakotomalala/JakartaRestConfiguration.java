e3d71275aacc4718c9fc9f57385747f184542164
27_Rakotomalala_Andomahefa_Julio <andomahefa.julio.r@gmail.com>
2023-03-14 15:50:16 +0300

    Etat initial

---
+++ src/main/java/mg/itu/tp1customer_27_rakotomalala/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package mg.itu.tp1customer_27_rakotomalala;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
