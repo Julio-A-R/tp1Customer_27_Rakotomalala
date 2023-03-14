c072d572e351ab81cb0f02653653378fd303e963
27_Rakotomalala_Andomahefa_Julio <andomahefa.julio.r@gmail.com>
2023-03-14 17:27:11 +0300

    Ajout EJB CustomerManager (rectif)

--- src/main/java/mg/itu/tp1customer_27_rakotomalala/ejb/CustomerManager.java
+++ src/main/java/mg/itu/tp1customer_27_rakotomalala/ejb/CustomerManager.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package mg.itu.tp1customer_27_rakotomalala.session;
+package mg.itu.tp1customer_27_rakotomalala.ejb;
 
 import mg.itu.tp1customer_27_rakotomalala.entities.Customer;
 import jakarta.ejb.Stateless;
