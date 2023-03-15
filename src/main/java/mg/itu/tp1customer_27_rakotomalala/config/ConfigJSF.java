06bf450de29eb42ce59fbab9582333c1fe4a5a07
27_Rakotomalala_Andomahefa_Julio <andomahefa.julio.r@gmail.com>
2023-03-15 09:17:28 +0300

    Configuration pour JSF

---
+++ src/main/java/mg/itu/tp1customer_27_rakotomalala/config/ConfigJSF.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mg.itu.tp1customer_27_rakotomalala.config;
+
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.faces.annotation.FacesConfig;
+
+/**
+ *
+ * @author 27_rakotomalala
+ */
+
+@ApplicationScoped
+@FacesConfig
+public class ConfigJSF {
+    
+}
